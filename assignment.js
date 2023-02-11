4f2f9f1b9e4be006b3dc4cd6b7dcd0ebd34c004c
Pyeonghwassi <82952143+MarionBraide@users.noreply.github.com>
2023-02-11 10:20:23 +0100

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,19 @@
+let numOne = Number(prompt("Enter the first number"));
+let numTwo = Number(prompt("Enter the second number"));
+let operation = prompt("Please choose an operation: +, -, *, / ");
+
+let result;
+
+if (operation === "+") {
+    result = numOne + numTwo;
+} else if (operation === "-") {
+    result = numOne - numTwo;
+} else if (operation === "*") {
+    result = numOne * numTwo;
+} else if (operation === "/") {
+    result = numOne / numTwo
+} else {
+    result = "This is not a valid operation";
+}
+
+console.log("The result is : " + result);
